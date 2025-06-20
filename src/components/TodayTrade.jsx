4e76184f0e52a73f85733f7d47d1adc5b0e7db23
ilkhomov-web <ilhomovelyor119@gmail.com>
2025-06-20 13:21:40 +0500

    some chnage

--- src/components/TodayTrade.jsx
+++ src/components/TodayTrade.jsx
@@ -3,9 +3,34 @@ import React from "react";
 
 function TodayTrade() {
   return (
-    <Box>
-      <Box>
-        <Box>
+    <Box
+      sx={{
+        background: "#f5f5f5",
+        padding: "10px",
+        borderRadius: "8px",
+        marginTop: "20px",
+      }}
+    >
+      <Box
+        sx={{
+          background: "rgb(200 16 158)",
+          padding: "10px",
+          borderRadius: "8px",
+        }}
+      >
+        <Box
+          sx={{
+            width: "15%",
+            padding: "10px",
+            borderRadius: "8px",
+            background: "white",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            alignItems: "center",
+            cursor: "pointer",
+          }}
+        >
           <Typography>Bugungi Savdo</Typography>
           <Typography>800.000 So'm</Typography>
         </Box>
